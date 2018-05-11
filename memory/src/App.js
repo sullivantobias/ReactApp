528d314c49db291456ab32ced435a9cd170ba8be
Tobias <githubst@gmail.com>
2018-05-11 17:05:46 +0200

    Init

---
+++ memory/src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import shuffle from 'lodash.shuffle'
+
+import './App.css'
+
+import Card from './Card'
+import GuessCount from './GuessCount'
+import HallOfFame, { FAKE_HOF } from './HallOfFame.js'
+
+const SIDE = 6
+const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿'
+
+class App extends Component {
+  cards = this.generateCards()
+
+  generateCards() {
+    const result = []
+    const size = SIDE * SIDE
+    const candidates = shuffle(SYMBOLS)
+    while (result.length < size) {
+      const card = candidates.pop()
+      result.push(card, card)
+    }
+    return shuffle(result)
+  }
+
+  handleCardClick(card) {
+    console.log(card, 'clicked')
+  }
+
+  render() {
+    const won = new Date().getSeconds() % 2 === 0
+    return (
+      <div className="memory">
+        <GuessCount guesses={0} />
+        <Card card="😀" feedback="hidden" onClick={this.handleCardClick} />
+        <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick} />
+        <Card
+          card="💖"
+          feedback="justMismatched"
+          onClick={this.handleCardClick}
+        />
+        <Card card="🎩" feedback="visible" onClick={this.handleCardClick} />
+        <Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
+        <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick} />
+        {won && <HallOfFame entries={FAKE_HOF} />}
+
+      </div>
+    )
+  }
+}
+
+export default App
